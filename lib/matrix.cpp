75a7dfb68743aa5a790d482ec633704119254cee
Andrew Luo <andrew.zhao.luo@gmail.com>
2023-10-29 21:53:51 -0700

    add print function to Matrix

--- lib/matrix.cpp
+++ lib/matrix.cpp
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <iostream>
 #include "matrix.h"
 
 namespace support {
@@ -37,10 +38,44 @@ namespace support {
     }
 
     template<typename T>
-    T& Matrix<T>::access(uint32_t r, uint32_t c) {
+    inline T& Matrix<T>::access(uint32_t r, uint32_t c) {
         // NOTE: does not check indices are valid, be careful!
         uint32_t flat_index = r * _width + c;
         return _data[flat_index];         
+    }
+
+    template<typename T>
+    inline T& Matrix<T>::a(uint32_t r, uint32_t c) {
+        return access(r, c);
+    }
+
+    template<typename T>
+    uint32_t Matrix<T>::get_height() const {
+        return _height;
+    };
+
+    template<typename T>
+    uint32_t Matrix<T>::get_width() const {
+        return _width;
+    }
+
+    template<typename T>
+    void print(std::ostream& os, const Matrix<T>& arr) {
+        static const uint32_t MAX_ROWS = 10;
+        static const uint32_t MAX_COLS = 10;
+
+        bool fit_rows = arr.get_height() <= MAX_ROWS;
+        bool fit_cols = arr.get_width() <= MAX_COLS;
+
+        for (int r = 0; r < std::min(MAX_ROWS, arr.get_height()); r++) {
+            for (int c = 0; c < std::min(MAX_COLS, arr.get_width()); c++) {
+                os << arr.a(r, c) << " ";
+            }
+
+            os << (fit_cols ? "" : "...") << std::endl;
+        }
+
+        os << (fit_rows ? "" : "...") << std::endl;;
     }
 
 } // namespace support
--- lib/matrix.h
+++ lib/matrix.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <iostream>
 
 #ifndef __MATRIX_H__
 #define __MATRIX_H__
@@ -19,8 +20,15 @@ public:
     Matrix(Matrix&&);            // move constructor
     Matrix& operator=(Matrix&&); // move assignment constructor
 
-    T& access(uint32_t r, uint32_t c); 
+    inline T& access(uint32_t r, uint32_t c); 
+    inline T& a(uint32_t r, uint32_t c); 
+
+    uint32_t get_height() const;
+    uint32_t get_width() const;
 };
+
+template <typename T>
+void print(std::ostream&, const Matrix<T>&);
 
 } // namespace support
 
