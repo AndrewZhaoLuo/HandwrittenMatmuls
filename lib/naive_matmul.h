c056826f271200e240f5832d12f31c1b11edbd12
Andrew Luo <andrew.zhao.luo@gmail.com>
2023-11-28 23:10:58 -0800

    basic tiled matmuls

--- lib/naive_matmul.h
+++ lib/naive_matmul.h
@@ -9,6 +9,7 @@ using support::Matrix;
 namespace algo {
 namespace naive {
 
+// TODO: switch N/M, M comes before M lol
 template <typename T>
 inline void verifyMatmul(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C, uint32_t &N,
                          uint32_t &M, uint32_t &K) {
@@ -24,6 +25,7 @@ inline void verifyMatmul(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C, uint32_t &N,
   // TODO, add optional assertions controlled by DEBUG flag
 }
 
+// STEP 1: The simplest implementation
 template <typename T>
 void naive_matmul_ijk(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C) {
   uint32_t N, M, K;
@@ -40,6 +42,8 @@ void naive_matmul_ijk(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C) {
   }
 }
 
+// STEP 2: Reordering loops leads to better memory reuse inner loop
+// only advances pointers in B.
 template <typename T>
 void naive_matmul_kij(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C) {
   uint32_t N, M, K;
@@ -57,6 +61,108 @@ void naive_matmul_kij(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C) {
       for (uint32_t j = 0; j < M; j++) {
         // TODO: possibly make this default constructor of T
         C.a(i, j) += A.r(i, k) * B.r(k, j);
+      }
+    }
+  }
+}
+
+// STEP 3: Rudimentary tiling helps with additional memory improvements
+template <typename T, size_t tileN, size_t tileM, size_t tileK>
+void tiled_ijk_matmul_ijk(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C) {
+  uint32_t N, M, K;
+  verifyMatmul(A, B, C, N, M, K);
+
+  for (uint32_t i = 0; i < N; i++) {
+    for (uint32_t j = 0; j < M; j++) {
+      C.a(i, j) = 0;
+    }
+  }
+
+  T tile_buffer[tileN][tileM];
+
+  // TODO: assertions on divisibility of tiling for now
+  for (uint32_t tile_i = 0; tile_i < N / tileN; tile_i++) {
+    for (uint32_t tile_j = 0; tile_j < M / tileM; tile_j++) {
+      for (uint32_t tile_k = 0; tile_k < K / tileK; tile_k++) {
+
+        // Zero tile_buffer
+        for (uint32_t i = 0; i < tileN; i++) {
+          for (uint32_t j = 0; j < tileM; j++) {
+            tile_buffer[i][j] = 0;
+          }
+        }
+
+        // ijk
+        for (uint32_t inner_i = 0; inner_i < tileN; inner_i++) {
+          for (uint32_t inner_j = 0; inner_j < tileM; inner_j++) {
+            for (uint32_t inner_k = 0; inner_k < tileK; inner_k++) {
+              uint32_t i = tile_i * tileN + inner_i;
+              uint32_t j = tile_j * tileM + inner_j;
+              uint32_t k = tile_k * tileK + inner_k;
+              tile_buffer[inner_i][inner_j] += A.r(i, k) * B.r(k, j);
+            }
+          }
+        }
+
+        // Write buffer back
+        for (uint32_t inner_i = 0; inner_i < tileN; inner_i++) {
+          for (uint32_t inner_j = 0; inner_j < tileM; inner_j++) {
+            uint32_t i = tile_i * tileN + inner_i;
+            uint32_t j = tile_j * tileM + inner_j;
+            C.a(i, j) = tile_buffer[inner_i][inner_j];
+          }
+        }
+      }
+    }
+  }
+}
+
+// STEP 4: As STEP 3, but with kij inner kernel we know is faster
+template <typename T, size_t tileN, size_t tileM, size_t tileK>
+void tiled_ijk_matmul_kij(Matrix<T> &A, Matrix<T> &B, Matrix<T> &C) {
+  uint32_t N, M, K;
+  verifyMatmul(A, B, C, N, M, K);
+
+  for (uint32_t i = 0; i < N; i++) {
+    for (uint32_t j = 0; j < M; j++) {
+      C.a(i, j) = 0;
+    }
+  }
+
+  T tile_buffer[tileN][tileM];
+
+  // TODO: assertions on divisibility of tiling for now
+  for (uint32_t tile_i = 0; tile_i < N / tileN; tile_i++) {
+    for (uint32_t tile_j = 0; tile_j < M / tileM; tile_j++) {
+      for (uint32_t tile_k = 0; tile_k < K / tileK; tile_k++) {
+
+        // Zero tile_buffer
+        for (uint32_t i = 0; i < tileN; i++) {
+          for (uint32_t j = 0; j < tileM; j++) {
+            tile_buffer[i][j] = 0;
+          }
+        }
+
+        // kij
+        for (uint32_t inner_k = 0; inner_k < tileK; inner_k++) {
+          for (uint32_t inner_i = 0; inner_i < tileN; inner_i++) {
+            for (uint32_t inner_j = 0; inner_j < tileM; inner_j++) {
+              uint32_t i = tile_i * tileN + inner_i;
+              uint32_t j = tile_j * tileM + inner_j;
+              uint32_t k = tile_k * tileK + inner_k;
+              tile_buffer[inner_i][inner_j] += A.r(i, k) * B.r(k, j);
+            }
+          }
+        }
+
+        // Write buffer back
+        for (uint32_t inner_i = 0; inner_i < tileN; inner_i++) {
+          for (uint32_t inner_j = 0; inner_j < tileM; inner_j++) {
+            uint32_t i = tile_i * tileN + inner_i;
+            uint32_t j = tile_j * tileM + inner_j;
+            C.a(i, j) = tile_buffer[inner_i][inner_j];
+          }
+        }
       }
     }
   }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
 #include "matrix.h"
 #include "naive_matmul.h"
+#include <chrono>
 #include <functional>
 #include <iostream>
 #include <random>
-#include <chrono>
 
 using support::Matrix;
 
@@ -22,8 +22,8 @@ void randomInitFloatMatrix(Matrix<float> &mat) {
 }
 
 template <int warmups, int repeats>
-int64_t benchmark_get_us(Matrix<float> &matA, Matrix<float> &matB, Matrix<float> &matC,
-               f_matmul_float func) {
+int64_t benchmark_get_us(Matrix<float> &matA, Matrix<float> &matB,
+                         Matrix<float> &matC, f_matmul_float func) {
   for (int i = 0; i < warmups; i++) {
     func(matA, matB, matC);
   }
@@ -47,20 +47,54 @@ void test_conditions() {
   randomInitFloatMatrix(matB);
   randomInitFloatMatrix(matC);
 
-  std::cout << "M = " << M << ", N = " << N << ", K = " << K << ", warmups = " << warmups << ", repeats " << repeats << std::endl;
+  std::cout << "M = " << M << ", N = " << N << ", K = " << K
+            << ", warmups = " << warmups << ", repeats " << repeats
+            << std::endl;
 
-  uint64_t us_ijk = benchmark_get_us<warmups, repeats>(matA, matB, matC, algo::naive::naive_matmul_ijk<float>);
+  uint64_t us_ijk = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::naive_matmul_ijk<float>);
   std::cout << "\tnaive_matmul_ijk (us): " << us_ijk << std::endl;
 
-  uint64_t us_kij = benchmark_get_us<warmups, repeats>(matA, matB, matC, algo::naive::naive_matmul_kij<float>);
+  uint64_t us_kij = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::naive_matmul_kij<float>);
   std::cout << "\tnaive_matmul_kij (us): " << us_kij << std::endl;
+
+  uint64_t us_tiled8ijk_inner_ijk = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::tiled_ijk_matmul_ijk<float, 8, 8, 8>);
+  std::cout << "\tiled_ijk_matmul_ijk<float, 8, 8, 8> (us): "
+            << us_tiled8ijk_inner_ijk << std::endl;
+
+  uint64_t us_tiled16_ijk_inner_ijk = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::tiled_ijk_matmul_ijk<float, 16, 16, 16>);
+  std::cout << "\tiled_ijk_matmul_ijk<float, 16, 16, 16> (us): "
+            << us_tiled16_ijk_inner_ijk << std::endl;
+
+  uint64_t us_tiled32_ijk_inner_ijk = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::tiled_ijk_matmul_ijk<float, 32, 32, 32>);
+  std::cout << "\tiled_ijk_matmul_ijk<float, 32, 32, 32> (us): "
+            << us_tiled32_ijk_inner_ijk << std::endl;
+
+  uint64_t us_tiled8_ijk_inner_kij = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::tiled_ijk_matmul_kij<float, 8, 8, 8>);
+  std::cout << "\tiled_ijk_matmul_kij<float, 8, 8, 8> (us): "
+            << us_tiled8_ijk_inner_kij << std::endl;
+
+  uint64_t us_tiled16_ijk_inner_kij = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::tiled_ijk_matmul_kij<float, 16, 16, 16>);
+  std::cout << "\tiled_ijk_matmul_kij<float, 16, 16, 16> (us): "
+            << us_tiled16_ijk_inner_kij << std::endl;
+
+  uint64_t us_tiled32_ijk_inner_kij = benchmark_get_us<warmups, repeats>(
+      matA, matB, matC, algo::naive::tiled_ijk_matmul_kij<float, 32, 32, 32>);
+  std::cout << "\tiled_ijk_matmul_kij<float, 32, 32, 32> (us): "
+            << us_tiled32_ijk_inner_kij << std::endl;
 }
 
 void example_simple() {
   std::cout << "Hello world!" << std::endl;
-  Matrix<float> matA(5, 5);
-  Matrix<float> matB(5, 5);
-  Matrix<float> matC(5, 5);
+  Matrix<float> matA(32, 32);
+  Matrix<float> matB(32, 32);
+  Matrix<float> matC(32, 32);
 
   randomInitFloatMatrix(matA);
   randomInitFloatMatrix(matB);
@@ -77,9 +111,19 @@ void example_simple() {
   algo::naive::naive_matmul_kij(matA, matB, matC);
   std::cout << "Matrix C naive outer product: " << std::endl;
   std::cout << matC << std::endl;
+
+  algo::naive::tiled_ijk_matmul_ijk<float, 16, 16, 16>(matA, matB, matC);
+  std::cout << "Matrix C tiled ijk inner ijk product: " << std::endl;
+  std::cout << matC << std::endl;
+
+  algo::naive::tiled_ijk_matmul_kij<float, 16, 16, 16>(matA, matB, matC);
+  std::cout << "Matrix C tiled ijk inner kij product: " << std::endl;
+  std::cout << matC << std::endl;
 }
 
 int main() {
+  example_simple();
+
   test_conditions<256, 256, 256, 1, 10>();
   test_conditions<512, 512, 512, 1, 10>();
   test_conditions<1024, 1024, 1024, 1, 10>();
